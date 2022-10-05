75b6ea80bc579a5aa04ee738e3e62ecdc0d4587a
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2022-10-05 13:24:37 +0300

    Создали 2 книги с разным количеством авторов

--- src/jptv21oopproba/JPTV21OOPProba.java
+++ src/jptv21oopproba/JPTV21OOPProba.java
@@ -5,6 +5,9 @@
  */
 package jptv21oopproba;
 
+import myclasses.Author;
+import myclasses.Book;
+
 /**
  *
  * @author Melnikov
@@ -15,6 +18,29 @@ public class JPTV21OOPProba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       Author author1 = new Author();
+       author1.setFirstname("Aleksandr");
+       author1.setLastname("Pushkin");
+       Book book = new Book();
+       book.setCaption("Jevgeny Onegin");
+       Author[] authors = new Author[1];
+       authors[0] = author1;
+       book.setAuthors(authors);
+        System.out.println(book.toString());
+        
+       Book book2 = new Book();
+       book2.setCaption("Zolotoy teljonok");
+       Author author2 = new Author();
+       author2.setFirstname("Ilja");
+       author2.setLastname("Ilf");
+       Author author3 = new Author();
+       author3.setFirstname("Jevgeny");
+       author3.setLastname("Petrov");
+       Author[] authors2 = new Author[2];
+       authors2[0]=author2;
+       authors2[1]=author3;
+       book2.setAuthors(authors2);
+        System.out.println(book2.toString());
        
     }
     
